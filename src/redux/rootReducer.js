d5cbd74376810e9601f80d6c2f587cb3f561d169
Dev-DavidPrabu <97029653+Dev-DavidPrabu@users.noreply.github.com>
2022-02-17 11:10:48 +0530

    Delete src directory

--- src/redux/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import  AuthReducer  from "./Auth/AuthReducer";
-import changeState from "./default/changeState";
-
-
-export const rootReducer=combineReducers({
-  auth:AuthReducer,
-  changeState:changeState
-})
