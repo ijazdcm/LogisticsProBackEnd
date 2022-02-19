858c3293351a28b858a4eeab06a295f235735380
mariavanaraj <mariavanaraj2@gmail.com>
2022-02-19 18:16:12 +0530

    Customer creation back end

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Customer\CustomerRequest;
 use App\Http\Resources\Customer\CustomerResource;
-use App\Models\Customer\Customer_info;
+// use App\Models\Customer\Customer_info;
 use Customer;
 use Illuminate\Http\Request;
 use App\Helper\File\FileHelper;
 use Illuminate\Support\Facades\Cache;
 use App\Http\Resources\ParkingYardGate\ParkingYardGateResource;
+use App\Models\Customer\Customer_info;
 use App\Models\ParkingYardGate\Parking_Yard_Gate;
 
 class CustomerController extends Controller
@@ -22,12 +23,15 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        return CustomerResource::collection(Cache::remember('Customer_info', now()->addDay(), function () {
+        // return CustomerResource::collection(Cache::remember('Customer_info', now()->addDay(), function () {
 
-            return Customer_info::with('bank_name')
-              ->get();
+        //     // return Customer_info::with('bank_name')
+        //     //   ->customer()
+        //     //   ->get();
+        //     // return CustomerResource::collection(Customer_info::all());
+        // }));
 
-        }));
+        return CustomerResource::collection(Customer_info::all());
     }
 
     /**
@@ -85,8 +89,11 @@ class CustomerController extends Controller
 
         if($customer)
         {
-            return new CustomerResource($customer->load('customer_id'));
+            // return new CustomerResource($customer->load('customer_id'));
+            // return  CustomerResource::make($customer);
+            return new CustomerResource($customer);
         }
+
         return response()->json(['message' => 'Customer Not found'], 404);
     }
 
@@ -97,10 +104,52 @@ class CustomerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $id,$action)
     {
-        //
-    }
+
+        $customer=Customer_info::where('customer_status',1)
+        ->where('id',$id)
+        ->first();
+        if($customer)
+        {
+
+
+          $customer=$action->handleUpdateImages($request,$customer);
+
+           $is_updated=$customer->update([
+            "customer_name" => $request->customer_name,
+            "customer_PAN_card_number" => $request->customer_PAN_card_number,
+            "customer_Aadhar_card_number" => $request->customer_Aadhar_card_number,
+            "customer_bank_id" => $request->customer_bank_id,
+            "customer_bank_branch" => $request->customer_bank_branch,
+            "customer_bank_ifsc_code" => $request->customer_bank_ifsc_code,
+            "customer_bank_account_number" => $request->customer_bank_account_number,
+            "customer_street_name" => $request->customer_street_name,
+             "customer_area" => $request->customer_area,
+             "customer_city" => $request->customer_city,
+            "customer_district" => $request->customer_district,
+            "customer_postal_code" => $request->customer_postal_code,
+            "customer_region" => $request->customer_region,
+            "customer_gst_number" => $request->customer_gst_number,
+            "customer_payment_terms" => $request->customer_payment_terms,
+             "customer_remarks" => $request->customer_remarks,
+
+           ]);
+
+           if($is_updated)
+           {
+              $updated_customer=Customer_info::where('customer_status',1)
+              ->where('id',$id)
+              ->first();
+               return new CustomerResource($updated_customer->load('customer_status'));
+           }
+
+
+
+        }
+        return response()->json(['message' => 'Something went wrong'],500);
+  }
+
 
     /**
      * Remove the specified resource from storage.
